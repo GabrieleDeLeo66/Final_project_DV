ce5556cc7651ca4aa2a5de855326b1e8c81fd531
GabrieleDeLeo66 <deleo.gabriele97@gmail.com>
2022-05-29 18:38:59 +0200

    "Colored Map"

--- src/javascript/Map.js
+++ src/javascript/Map.js
@@ -6,22 +6,44 @@
   height = +svg.attr("height");
 
   // Map and projection
-  var projection = d3.geoNaturalEarth1()
-  .scale(width / 1.3 / Math.PI)
-  .translate([width / 2, height / 2])
+  var path = d3.geoPath();
+  var projection = d3.geoAiry()
+  .scale(1000)
+  .translate([400, 1140])
+
+  
+// Data and color scale
+  var data = d3.map();
+  var colorScale = d3.scaleThreshold()
+  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .range(d3.schemeBlues[7]);
 
   // Load external data and boot
-  d3.json("/src/asset/europe.geo.json", function(data){
+    d3.queue()
+    .defer(d3.json, "/src/asset/europe.geo.json")
+    .defer(d3.csv, "/data/owid-covid-data-Europe.csv", function(d) { data.set(d.iso_code, +d.total_cases); })
+    .await(ready);
+
 
   // Draw the map
-  svg.append("g")
+  function ready(error, topo) {
+
+    // Draw the map
+    svg.append("g")
       .selectAll("path")
-      .data(data.features)
-      .enter().append("path")
-          .attr("fill", "#69b3a2")
-          .attr("d", d3.geoPath()
-              .projection(projection)
-          )
-          .style("stroke", "#fff")
-  })
+      .data(topo.features)
+      .enter()
+      .append("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+          .projection(projection)
+        )
+        // set the color of each country
+        .attr("fill", function (d) {
+            console.log(d)
+          d.total = data.get(d.properties.iso_a3) || 0;
+          console.log(d.properties.iso_a3)
+          return colorScale(d.total);
+        });
   }
+}
