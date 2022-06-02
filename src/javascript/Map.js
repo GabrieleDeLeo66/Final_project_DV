6855726409c04c90b0084d576584f449912c08b6
Your Name <you@example.com>
2022-06-02 17:43:05 +0200

    "tooltip funzionante"

--- src/javascript/Map.js
+++ src/javascript/Map.js
@@ -8,8 +8,8 @@
   // Map and projection
   var path = d3.geoPath();
   var projection = d3.geoAiry()
-  .scale(1000)
-  .translate([400, 1140]);
+  .scale(1150)
+  .translate([400, 1320]);
 
   
 // Data and color scale
@@ -27,22 +27,20 @@
   // Load external data and boot
     d3.queue()
     .defer(d3.json, "/src/asset/europe.geo.json")
-    .defer(d3.csv, "/data/owid-covid-data-Europe.csv", function(d) { data.set(d.iso_code, +d.new_cases); })
+    .defer(d3.csv, "/data/owid-covid-data-Europe.csv", function(d) { data.set(d.iso_code,+d.new_cases); })
     .await(ready);
+
+    d3.select('body')
+    .append('div')
+    .attr('id', 'tooltip')
+    .attr('style', 'position: absolute; opacity: 0;');
+
 
 
   // Draw the map
   function ready(error, topo) {
 
-
-    var mouseMove = function(d) {
-
-      Tooltip
-        .html(d.iso_code + "<br>" + "New cases: " + d.new_cases )
-        .style("left", (d3.mouse(this)[0]+10) + "px")
-        .style("top", (d3.mouse(this)[1]) + "px")
-    }
-
+    console.log(data)
     var  mouseOver = function(d) {
         d3.selectAll(".Country")
           .transition()
@@ -52,10 +50,9 @@
           .transition()
           .duration(200)
           .style("opacity", 1)
-          .style("stroke", "black")
-        
 
-          Tooltip.style("opacity", 1)
+          d3.select('#tooltip').transition().duration(200).style('opacity', 1).text(d)
+
       }
     
       var mouseLeave = function(d) {
@@ -68,7 +65,7 @@
           .duration(200)
           .style("stroke", "white")
 
-        Tooltip.style("opacity", 0)
+          d3.select('#tooltip').style('opacity', 0)
       }
 
 
@@ -86,24 +83,32 @@
         // set the color of each country
         .attr("fill", function (d) {
           d.total = data.get(d.properties.iso_a3) || 0;
+          console.log(d.properties.geounit)
           var fever = (Math.log((d.total/1000)+1)+36.5)
           console.log(fever)
           return colorScale(fever);
         })
         .attr("class", function(d){ return "Country" } )
-        .on("mouseover", mouseOver )
+        .on("mouseover", mouseOver = function(d) {
+            d3.selectAll(".Country")
+              .transition()
+              .duration(200)
+              .style("opacity", .2)
+            d3.select(this)
+              .transition()
+              .duration(200)
+              .style("opacity", 1)
+    
+              d3.select('#tooltip').transition().duration(200).style('opacity', 1).text(d.properties.geounit)
+    
+          })
         .on("mouseleave", mouseLeave )
-        .on("mousemove", mouseMove)
-
-        var Tooltip = d3.selectAll(".Country")
-        .append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 1)
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
+        .on('mousemove', function() {
+            d3.select('#tooltip')
+            .style('left', (d3.event.pageX+10) + 'px')
+            .style('top', (d3.event.pageY+10) + 'px')
+            })
+        
 
   }
 }
