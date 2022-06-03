08ace630836e3e8a0634b2fa34d17f25202e5f27
Your Name <you@example.com>
2022-06-03 14:31:28 +0200

    Commit

--- src/javascript/Map.js
+++ src/javascript/Map.js
@@ -1,7 +1,8 @@
 function Map() {
   // The svg
   var svg = d3.select("#map").append("svg").attr("id", "main");
-  (width = +svg.attr("width","1000px")), (height = +svg.attr("height","1000px"));
+  (width = +svg.attr("width", "1000px")),
+    (height = +svg.attr("height", "1000px"));
 
   // Map and projection
   var path = d3.geoPath();
@@ -9,12 +10,20 @@ function Map() {
 
   // Data and color scale
   var temperatures = [];
-  for (var i = 36; i < 42.1; i = i + 0.1) {
+  for (var i = 36.5; i < 43; i = i + 0.25) {
     temperatures.push(i);
   }
   console.log(temperatures);
   var data = d3.map();
-  var colorScale = d3.scaleLinear().domain([36, 42]).range(["white", "red"]);
+  var colors = d3.scaleLinear().domain([0, 24]).range(["white", "#C40000"]);
+  var c = [];
+  for (var i = 0; i < 25; i++) c.push(colors(i));
+  console.log(c);
+  var colorScale = d3.scaleQuantize().domain([36.5, 42]).range(c);
+  temperatures.forEach(function (item) {
+    console.log(colorScale(item));
+  });
+  console.log(colorScale(41.5));
 
   // Load external data and boot
   d3.queue()
@@ -26,17 +35,20 @@ function Map() {
 
   d3.select("#map")
     .append("input")
-    .attr("type","date")
-    .attr("id","date")
-    .attr("value","1997-01-07")
-    .attr("style","position:absolute;top:350px;left:50px;font-family:Montserrat;font-size:large;background-color:#4C4949;color:#fff;")
+    .attr("type", "date")
+    .attr("id", "date")
+    .attr("value", "1997-01-07")
+    .attr(
+      "style",
+      "position:absolute;top:350px;left:50px;font-family:Montserrat;font-size:large;background-color:#1e88cf;color:#fff;border-color:#333"
+    );
 
   d3.select("body")
     .append("div")
     .attr("id", "tooltip")
     .attr(
       "style",
-      "position: absolute; opacity: 0; width: 170px;background-color: #4C4949;color: #fff;text-align: center;padding: 5px 0;border-radius: 6px;font-family: Montserrat;"
+      "position: absolute; opacity: 0; width: 170px;background-color: #1e88cf;color: #fff;text-align: center;padding: 5px 0;border-radius: 6px;font-family: Montserrat;"
     );
 
   // Draw the map
@@ -66,14 +78,14 @@ function Map() {
       .append("path")
       // draw each country
       .attr("d", d3.geoPath().projection(projection))
-      .style("stroke", "white")
+      .style("stroke", "#E2E2E2")
       // set the color of each country
       .attr("fill", function (d) {
         d.total = data.get(d.properties.iso_a3) || 0;
         console.log(d.properties.geounit);
         var fever = Math.log(d.total / 1000 + 1) + 36.5;
-        console.log(fever);
-        return colorScale(fever);
+        console.log(arrotondaNumero(fever, 1));
+        return colorScale(arrotondaNumero(fever, 1));
       })
       .attr("class", function (d) {
         return "Country";
